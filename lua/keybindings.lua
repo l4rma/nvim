4eb17110d5cc5e309d91d9c71fb613816d1a15f2
Lars Magelssen <lars.magelssen@gmail.com>
2023-05-25 14:07:08 +0200

    cleaned up keybindings a bit

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,24 +1,63 @@
--- Key Mapping
-vim.g.mapleader = " "
-local keymap = vim.api.nvim_set_keymap
+--   _  __            __  __                   _             
+--  | |/ /___ _   _  |  \/  | __ _ _ __  _ __ (_)_ __   __ _ 
+--  | ' // _ \ | | | | |\/| |/ _` | '_ \| '_ \| | '_ \ / _` |
+--  | . \  __/ |_| | | |  | | (_| | |_) | |_) | | | | | (_| |
+--  |_|\_\___|\__, | |_|  |_|\__,_| .__/| .__/|_|_| |_|\__, |
+--            |___/               |_|   |_|            |___/ 
 
+-- Helper functions
+local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 local function vkeymap(key, map)
 	keymap('v', key, map, opts)
 end
+
 local function nkeymap(key, map)
 	keymap('n', key, map, opts)
 end
--- Save, Quit, Source, Alpha
-keymap('n', '<leader>w', ':w<CR>', {})
-keymap('n', '<leader>q', ':q<CR>', {})
-keymap('n', '<leader>wq', ':wq<CR>', {})
-keymap('n', '<leader>s', ':so %<CR>', {})
-keymap('n', '<c-n>', ':Alpha<cr>', opts)
 
--- Misc
-keymap('n', '<leader>v', 'ggVG', opts)
-keymap('n', 'Y', 'yy', opts)
+-- Set leader key to space
+vim.g.mapleader = ' '
+
+-- Save, Quit, Source, Alpha
+nkeymap('<leader>w', ':w<CR>')
+nkeymap('<leader>q', ':q<CR>')
+nkeymap('<leader>wq', ':wq<CR>')
+nkeymap('<leader>s', ':so %<CR>')
+nkeymap('<c-n>', ':Alpha<cr>')
+
+-- Highlight all
+nkeymap('<leader>v', 'ggVG')
+
+-- Yank line
+nkeymap('Y', 'yy')
+
+-- Better window navigation
+nkeymap("<C-h>", "<C-w>h")
+nkeymap("<C-j>", "<C-w>j")
+nkeymap("<C-k>", "<C-w>k")
+nkeymap("<C-l>", "<C-w>l")
+
+-- Resize windows with arrows
+nkeymap("<C-Up>", ":resize -2<CR>")
+nkeymap("<C-Down>", ":resize +2<CR>")
+nkeymap("<C-Left>", ":vertical resize -2<CR>")
+nkeymap("<C-Right>", ":vertical resize +2<CR>")
+
+-- Naviagate buffers
+nkeymap("<S-l>", ":bnext<CR>")
+nkeymap("<S-h>", ":bprevious<CR>")
+
+-- Surround highlighted text
+vkeymap('"', 's""hp')
+vkeymap('*', 's**hp')
+vkeymap('(', 's()hp')
+vkeymap('[', 's[]hp')
+vkeymap('{', 's{}hp')
+vkeymap(')', 's(  )hhp')
+vkeymap(']', 's[  ]hhp')
+vkeymap('}', 's{  }hhp')
+vkeymap('\'', 's\'\'hp')
 
 -- shift+k in visual mode open man page for highlighted word
 -- Moving text lines in visual, insert and normal mode
@@ -30,33 +69,27 @@ keymap('n', 'Y', 'yy', opts)
 --keymap('n', '<leader>k', ":m .-2<CR>==", opts)
 
 -- Copy highlighted text to Clipboard
-keymap('v', '<leader>c', '"+y', opts)
+vkeymap('<leader>c', '"+y')
 
 -- Copy filename to clipboard
 nkeymap('<leader>cf', ':let @+ = expand("%")<cr>')
 
 -- Paste last yanked
-keymap('n', '<leader>p', '"0p', opts)
-keymap('v', '<leader>p', '"0p', opts)
+nkeymap('<leader>p', '"0p')
+vkeymap('<leader>p', '"0p')
 
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- Remove Ansi color escape code
+nkeymap('<leader>rc', ':%s/\\e\\[[0-9;]*m//g<cr>')
 
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Go to definition in vertical split
+nkeymap('gsd', ':only<bar>vsplit<CR>gd')
 
--- Naviagate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-------------
+-- Plugins --
+-------------
 
 -- Nerdtree
-keymap("n", "<leader>n", ":NERDTreeToggle<cr>", opts)
+nkeymap("<leader>n", ":NERDTreeToggle<cr>")
 
 -- Git (vim-fugitive)
 nkeymap('<leader>gs', ':G<cr>')
@@ -65,7 +98,6 @@ nkeymap('<leader>gp', ':Git push<cr>')
 nkeymap('<leader>gh', ':diffget //2<cr>')
 nkeymap('<leader>gl', ':diffget //3<cr>')
 
-
 -- Telescope
 nkeymap('<leader>t', ':lua require("telescope.builtin").find_files()<cr>')
 nkeymap('<leader>fg', '<cmd>Telescope live_grep<cr>')
@@ -73,7 +105,7 @@ nkeymap('<leader><tab>', '<cmd>Telescope buffers<cr>')
 nkeymap('<leader>fh', '<cmd>Telescope help_tags<cr>')
 nkeymap('<leader>fm', '<cmd>Telescope man_pages<cr>')
 
--- LSP Mappings
+-- LSP
 nkeymap('gd', ':lua vim.lsp.buf.definition()<cr>')
 nkeymap('gD', ':lua vim.lsp.buf.declaration()<cr>')
 nkeymap('gi', ':lua vim.lsp.buf.implementation()<cr>')
@@ -87,11 +119,6 @@ nkeymap('<leader>af', ':lua vim.lsp.buf.code_action()<cr>')
 nkeymap('<a-cr>', ':lua vim.lsp.buf.code_action()<cr>')
 nkeymap('<leader>rn', ':lua vim.lsp.buf.rename()<cr>')
 
-nkeymap('gsd', ':only<bar>vsplit<CR>gd')
-
--- Remove Ansi color escape code
-nkeymap('<leader>rc', ':%s/\\e\\[[0-9;]*m//g<cr>')
-
 -- VimWiki
 -- TODO: Add shortcuts for: insert table
 nkeymap('<leader>it', ':VimwikiTable 3 3<cr>')
@@ -102,6 +129,3 @@ nkeymap('<leader>ff', ':TZFocus<cr>')
 
 -- Cellular automation
 nkeymap('<leader>fml', '<cmd>CellularAutomaton make_it_rain<CR>')
-
--- Misc/Unsorted
-vkeymap('"', 's""hp')
