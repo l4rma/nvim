147e8e7f26983632aa6bd3fd461decb0363d3740
Lars Magelssen <lars.magelssen@gmail.com>
2023-06-06 14:32:19 +0200

    added markdown previewer

--- lua/packages.lua
+++ lua/packages.lua
@@ -121,6 +121,15 @@ return require('packer').startup(function(use)
 		end
 	}
 
+	-- Markdown Preview
+	-- install without yarn or npm
+	--use({
+	--	"iamcco/markdown-preview.nvim",
+	--	run = function() vim.fn["mkdp#util#install"]() end,
+	--})
+
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 	-- Automatically set up configuration after cloning packer.nvim
 	if packer_bootstrap then
 		require('packer').sync()
