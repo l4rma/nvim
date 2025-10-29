9e6db5e8814bb2f4ff3f8858fbf661c1cd4c2c0e
Lars Magelssen <lars.magelssen@sparebank1.no>
2025-10-29 13:22:44 +0100

    disabled lots to make nvim 11 run

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,8 @@
+return {
+  "neanias/everforest-nvim",
+  config = function()
+    require("everforest").setup({
+      -- Your config here
+    })
+  end,
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,3 @@
 return {
 	'neovim/nvim-lspconfig',
-	config = function()
-		local lspconfig = require('lspconfig')
-		lspconfig.lua_ls.setup {
-			settings = {
-				Lua = {
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = {'vim'},
-					},
-				},
-			},
-		}
-		lspconfig.rust_analyzer.setup{}
-		lspconfig.hls.setup{}
-		lspconfig.gopls.setup{}
-		lspconfig.terraformls.setup{}
-		lspconfig.html.setup{}
-		lspconfig.pyright.setup{}
-	end
 }
